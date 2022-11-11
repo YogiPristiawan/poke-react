5cc339f04abed11fba3e1d61e07328eced94b550
yogipristiawan <yogistanada252@gmail.com>
2022-11-11 21:43:54 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-no-comment-textnodes */
+function App() {
+  return (
+    <div className="px-[20%] pt-4">
+      <p className="font-bold">hello</p>
+    </div>
+  );
+}
+
+export default App;
